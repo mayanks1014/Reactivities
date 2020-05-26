2eabad3881be8f27d52fba56162969c28ef16548
Mayank Sharma <mayanks1014@gmail.com>
2020-05-26 21:10:39 +0530

    initialCommit

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(new Value { Id = 1, Name = "Value101"}, new Value { Id= 2, Name= "Value102"});
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Reactivities/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Reactivities.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ValuesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public  async Task<ActionResult<IEnumerable<Value>>> GetAllValues()
+        {
+            var Values = await _context.Values.ToListAsync();           
+            return Ok(Values);
+        }
+
+        [HttpGet("{id}")]
+        public Value GetValueById(int id)
+        {
+            return new Value { Id=1,Name ="Mayank" };
+        }
+
+    }
+}
